37a1c516c235be68e1e7de9393ffce046a04b2ac
sidmn <sidmantri3@gmail.com>
2025-01-28 22:51:23 +0530

    test3

--- src/app/api/tmdb.tsx
+++ src/app/api/tmdb.tsx
@@ -1,5 +1,16 @@
 const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
+interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+  popularity: number;
+  release_date: string;
+  backdrop_path: string;
+  tagline: string;
+  overview: string;
+}
+
 export async function fetchTrending() {
   const res = await fetch(
     `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`,
@@ -13,16 +24,14 @@ export async function fetchTrending() {
   return res.json();
 }
 
-export async function fetchMovie(movieId) {
+export async function fetchMovie(movieId: string): Promise<Movie> {
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`,
+    { next: { revalidate: 3600 } } 
   );
 
   if (!res.ok) {
-    throw (
-      (new Error("Failed to fecth Movie details!"),
-      { next: { revalidate: 3600 } })
-    );
+    throw new Error("Failed to fetch Movie details!");
   }
 
   return res.json();
